459cb765a2d9db250fa963b4269448cec0455ec4
hydrae-soft <torres.lf21@gmai.com>
2023-05-26 17:58:24 -0300

    Making code concise.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
-// REFRESH GRAPHICS
 const cardholder_text = document.querySelector("span.cardholder")
 const card_number_text = document.querySelector("div.card-number")
 const card_exp_text = document.querySelector("span.card-exp")
 const card_cvc_text = document.querySelector("span.card-cvc")
-
+// Gets the form fields values when user typing.
 function onInputEvent(event, callback) {
   let value = event.target.value
   callback(value)
 }
+// Shows warnings to the users in case their input is wrong.
+function setValidInput(isValid, input, p_index) {
+  if (isValid) {
+    input.classList.remove("invalid-input")
+    invalid_inputs_p[p_index].classList.remove("invalid-input")  
+  }
+  else {
+    input.classList.add("invalid-input")
+    invalid_inputs_p[p_index].classList.add("invalid-input")
+  }
+} 
+// REFRESH GRAPHICS
 const refreshCardholder = value => cardholder_text.innerHTML = value
 const refreshCardNumber = value => {
   if (value.length <= 16) card_number_text.innerHTML = value.replace(/(\d{4})/g, "$1 ")
@@ -28,9 +39,7 @@ const refreshCVC = value => {
 const REGEX = {
   CARDHOLDER: /^[a-zA-Z]+( [a-zA-Z]+)+?$/,
   CARD_NUMBER: /^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/,
-  EXP_MM: /^[0-1][1-9]$/,
-  EXP_YY: /^[2-3][3-9]$/,
-  CVC: /^\d{3,4}$/,
+  CVC: /^\d{3,4}$/
 }
 const VALID_INPUTS = {
   CARDHOLDER: null,
@@ -41,71 +50,33 @@ const VALID_INPUTS = {
 }
 const invalid_inputs_p = document.querySelectorAll("form p")
 
-const validateCardholder = (value, input) => {
-  const p_index = 0
-  if (value.match(REGEX.CARDHOLDER)) {
-    VALID_INPUTS.CARDHOLDER = value
-    input.classList.remove("invalid-input")
-    invalid_inputs_p[p_index].classList.remove("invalid-input")
-  } else {
-    input.classList.add("invalid-input")
-    invalid_inputs_p[p_index].classList.add("invalid-input")
+function validateWithRegExp(value, regex, validInputs) {
+  if (value.match(regex) !== null) {
+    validInputs = value
+    return true
   }
+  return false
 }
-const validateCardNumber = (value, input) => {
-  const p_index = 1
-  if (value.match(REGEX.CARD_NUMBER)) {
-    VALID_INPUTS.CARD_NUMBER = parseInt(value)
-    input.classList.remove("invalid-input")
-    invalid_inputs_p[p_index].classList.remove("invalid-input")
-  } else {
-    input.classList.add("invalid-input")
-    invalid_inputs_p[p_index].classList.add("invalid-input")
-  }
-}
-const validateExpMM = (value, input) => {
-  // Get the first two digits only.
+function validateExpMM(value) {
   value = parseInt(value)
-  const p_index = 2
-  if (isNaN(value)) {
-    input.classList.add("invalid-input")
-    invalid_inputs_p[p_index].classList.add("invalid-input")
-  } else if (value <= new Date().getMonth() + 1 || value > 12) {
-    input.classList.add("invalid-input")
-    invalid_inputs_p[p_index].classList.add("invalid-input")
-  } else {
-    VALID_INPUTS.EXP_MM = value - 1
-    input.classList.remove("invalid-input")
-    invalid_inputs_p[p_index].classList.remove("invalid-input")
-  }
+  if (isNaN(value))
+    return false
+  else if (value <= new Date().getMonth() + 1 || value > 12)
+    return false
+  
+  VALID_INPUTS.EXP_MM = value - 1
+  return true
 }
-const validateExpYY = (value, input) => {  
-  // Get the first two digits only.
+function validateExpYY(value) {  
   value = parseInt(value)
-  const p_index = 2
   const this_year = new Date().getFullYear() - 2000
-  if (isNaN(value)) {
-    input.classList.add("invalid-input")
-    invalid_inputs_p[p_index].classList.add("invalid-input")
-  } else if (value < this_year || value > this_year + 6) {
-    input.classList.add("invalid-input")
-    invalid_inputs_p[p_index].classList.add("invalid-input")
-  } else {
-    VALID_INPUTS.EXP_YY = value + 2000
-    input.classList.remove("invalid-input")
-    invalid_inputs_p[p_index].classList.remove("invalid-input")
-  }
-}
-const validateCVC = (value, input) => {
-  const p_index = 3
-  if (value.match(REGEX.CVC)) {
-    VALID_INPUTS.CVC = value
-    input.classList.remove("invalid-input")
-    invalid_inputs_p[p_index].classList.remove("invalid-input")
-  } else {
-    input.classList.add("invalid-input")
-    invalid_inputs_p[p_index].classList.add("invalid-input")
-  }
+  if (isNaN(value))
+    return false
+  else if (value < this_year || value > this_year + 6)
+    return false
+  
+  VALID_INPUTS.EXP_YY = value + 2000
+  return true
 }
 // /VALIDATE USER INPUT
 const inputs = document.querySelectorAll("form .input-field")
@@ -115,7 +86,7 @@ inputs.forEach((input) => {
       input.addEventListener("input", event => {
         onInputEvent(event, value => {
           refreshCardholder(value)
-          validateCardholder(value, input)
+          setValidInput(validateWithRegExp(value, REGEX.CARDHOLDER, VALID_INPUTS.CARDHOLDER), input, 0)
         })
       })
       break
@@ -123,7 +94,7 @@ inputs.forEach((input) => {
       input.addEventListener("input", event => {
         onInputEvent(event, value => {
           refreshCardNumber(value)
-          validateCardNumber(value, input)
+          setValidInput(validateWithRegExp(value, REGEX.CARD_NUMBER, VALID_INPUTS.CARD_NUMBER), input, 1)
         })
       })
       break
@@ -131,7 +102,7 @@ inputs.forEach((input) => {
       input.addEventListener("input", event => {
         onInputEvent(event, value => {
           refreshExpDate(value, input)
-          validateExpMM(value, input)
+          setValidInput(validateExpMM(value), input, 2)
         })
       })
       break
@@ -139,7 +110,7 @@ inputs.forEach((input) => {
       input.addEventListener("input", event => {
         onInputEvent(event, value => {
           refreshExpDate(value, input)
-          validateExpYY(value, input)
+          setValidInput(validateExpYY(value), input, 2)
         })
       })
       break
@@ -147,7 +118,7 @@ inputs.forEach((input) => {
       input.addEventListener("input", event => {
         onInputEvent(event, value => {
           refreshCVC(value, input)
-          validateCVC(value, input)
+          setValidInput(validateWithRegExp(value, REGEX.CVC, VALID_INPUTS.CVC), input, 3)
         })
       })
   }
